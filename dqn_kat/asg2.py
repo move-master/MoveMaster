ceeaf6f0bdde342ff4576d482880e24cc0dc1742
Katrina Mosquera-Maykovych <katrinamosquera@gmail.com>
2025-10-24 21:00:23 -0400

    Added Notes about New Rep Specs; Few changes to asg2.py

--- dqn_kat/asg2.py
+++ dqn_kat/asg2.py
@@ -1,20 +1,21 @@
 class InitializationError(Exception):
     pass
 
+# Tower objects represent jenga tower states. A basic check is performed to ensure that the initial state length is divisible by 3,
 class Tower:
-    def __init__(self, height : int, state : tuple):
-        self.height = height
+    def __init__(self, state : tuple):
         self.state = state
         if len(state) % 3 != 0:
             raise InitializationError("Length of state should be divisible by 3.")
-        elif height*3 != len(state):
-            raise InitializationError("Length of state must be equal to height*3.")
-
+        
     def __repr__(self):
 
         result = f"Tower({self.height}, {self.state})"
         return result
     
+    # Prints a "pretty" visualization of the tower, where each layer is represented as a list of three elements.
+    # Printed from the top down, so last layer corresponds to bottom of the tower.
+    # Layers printed from left to right, as they are initialized in tower.state.
     def pretty(self):
         counter = 0
         layer = []
@@ -26,7 +27,7 @@ class Tower:
                 print(layer)
                 layer = []
 
-
+# Returns a list of three bits that correspond to a layer on the tower at index == num.
 def getLayer(tower : Tower, num : int):
     counter = 0
     groupOf3 = []
@@ -39,32 +40,38 @@ def getLayer(tower : Tower, num : int):
             layers.append(groupOf3)
             groupOf3 = []
     return layers[num]
-    
+
+# Basic validator for Jenga tower states. It checks to ensure that the tower is theoretically stable, has no empty layers, and the
+# layer second from the top is complete (under the official rules of Jenga, this layer should always be complete at the start of each turn).
+# Additionally, checks to make sure that the total number of blocks in the tower is divisible by 3.
+# The validator is not capable of checking for invalid/impossible state transitions, however.
 def isValid(tower : Tower, flag : bool):
+    height = len(tower.state)//3
     result = False
     totalBlocks = 0
-    topLayer = getLayer(tower, tower.height-1)
-    for layer in range(tower.height):
+    for layer in range(height):
         currLayer = getLayer(tower, layer)
         for block in currLayer:
             totalBlocks += block
-        print(f"currLayer == {currLayer}")
-        if currLayer == [0,0,1] or currLayer == [1,0,0] or currLayer == [0,0,0]:
-            if layer == tower.height-1:
+        if currLayer == [0,0,1] or currLayer == [1,0,0]:
+            if layer == height-1:
                 result = True
             else:
                 if(flag): {print(f"INVALID b/c encountered unstable layer")}
                 return False
         elif currLayer == [0,1,0] or currLayer == [1,0,1] or currLayer == [0,1,1] or currLayer == [1,1,0]:
-            if layer != tower.height-2:
+            if layer != height-2:
                 result = True
             else:
                 if(flag): {print(f"INVALID b/c cannot remove blocks from layer directly below incomplete top layer")}
                 return False
+        elif currLayer == [0,0,0]:
+            if(flag): {print(f"INVALID b/c cannot have entirely empty layer.")}
+            return False
         else:
             result = True
-    if totalBlocks != 54:
-        if(flag): {print(f"INVALID b/c totalBlocks is {totalBlocks}")}
+    if totalBlocks % 3 != 0:
+        if(flag): {print(f"INVALID b/c totalBlocks is {totalBlocks}, which is NOT divisible by 3.")}
         return False
     return result
 
@@ -73,12 +80,15 @@ def isValid(tower : Tower, flag : bool):
 
 if __name__ == "__main__":
     print("Running asg2.py...")
-    state = (1,1,0,0,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,0,1,0,0,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1)
+    
+    # The following state, while it passes the validator, is not possible to achieve through legal Jenga moves.
+    state = (0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,0,0)
+
+    tower = Tower(state)
+    tower.pretty()
+    print(f" is tower valid? >> {isValid(tower, True)}")
     totalBlocks = 0
     for block in state:
         totalBlocks += block
     print(F"num blocks: {totalBlocks}")
-    height = len(state)//3
-    tower = Tower(height,state)
-    print(f" is tower valid? >> {isValid(tower, True)}")
-    tower.pretty()
+    print(F"height: {len(state)//3}")
